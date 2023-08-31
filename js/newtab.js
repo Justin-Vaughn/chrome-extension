0c82c8cb31fb57531f39b0a638ef547000c8586c
Justin Vaughn <justinvsoccer39@gmail.com>
2023-08-31 13:08:40 -0400

    implemented struct for greeting

--- js/newtab.js
+++ js/newtab.js
@@ -1,3 +1,6 @@
+// <------------ CLOCK ------------>
+
+// returns a formated time for the clock
 const time = () => {
     const date = new Date();
     const time = date.getHours() + ":" + date.getMinutes();
@@ -8,6 +11,24 @@ const time = () => {
 document.getElementById("clock-text").innerHTML = time();
 
 // automatically updates the time every 1 sec
-setInterval(function() {
+setInterval(function () {
     document.getElementById("clock-text").innerHTML = time();
-  }, 1000);
+}, 1000);
+
+// <------------ GREETING ------------>
+
+// retrieves the messages stored in chrome, if there is none, default is '(name)'
+
+// if storage is empty
+let message = "Hello, (name)";
+
+// if storage is not emplty
+
+
+
+
+// displays the message
+document.getElementById("greeting-text").innerHTML = message;
+
+// user is able to edit the message
+
